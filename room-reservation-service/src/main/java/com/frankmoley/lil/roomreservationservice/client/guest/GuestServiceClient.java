da8ea75a36cd0686d581c1d87e770e0edb6e0149
Huzefa Zoaib <huzefa_zoaib2000@yahoo.com>
2024-08-19 00:13:41 -0400

    API Gateway Implemented. And applied for Guest Service. Guest Service Requests are flowing through API Gateway

--- room-reservation-service/src/main/java/com/frankmoley/lil/roomreservationservice/client/guest/GuestServiceClient.java
+++ room-reservation-service/src/main/java/com/frankmoley/lil/roomreservationservice/client/guest/GuestServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "guest-service", fallback = GuestServiceFallback.class)
+@FeignClient(value = "api-gateway-service", fallback = GuestServiceFallback.class)
 public interface GuestServiceClient {
 
     final static String GUESTS_URL_PART = "/guests";
